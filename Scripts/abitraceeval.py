2745752a4f7c8638a6bdba4cb0e0240c1859ef3c
DavidBulger <davidabulger@gmail.com>
2014-07-07 06:29:25 -0400

    Input with SeqIO and fixed reverse eval
    Input uses new AbiIO and SeqRecord files.
    Fixed reverse evaluation of mutations
    Added work-around for fwo (line 226)

--- Scripts/abitraceeval.py
+++ Scripts/abitraceeval.py
@@ -12,7 +12,7 @@ Sequence called heterozygous or homozygous for mutation.
 import sys
 import os
 import csv
-from Bio import AbiTracer
+from Bio import SeqIO
 from Bio.Seq import Seq
 
 # Set working directory
@@ -52,6 +52,15 @@ if StoreTest is "Y":
             Store = i
         else: i+=1
     print "Next Available Storage Site:" + str(Store)
+"""
+# Generate 2D array to store mutation data (currently stores up to 100)
+# Header
+mutation = [[0 for x in xrange(4)] for x in xrange(101)] 
+mutation[0][0] = "Mutation" # Mutation Name
+mutation[0][1] = "Sequence" # 20 bp upstream sequence
+mutation[0][2] = "WTbp" # Wild-Type Base at Mutation Site
+mutation[0][3] = "MTbp" # Mutant Base at Mutation Site
+"""
 
 # Input new mutation information
 print "Do you want to add a new mutation to the search database?"
@@ -150,23 +159,26 @@ def print_details(file_name,data1,data2,data3,data4,phd,WTf,lgkf,gkf,fwo,pos):
     return
     
 #Reverse
-def print_rdetails(file_name,data1,data2,data3,data4,phd,WTrF,lgkrF,fwo):
-    gkbp = seq[lgkrF-1:lgkrF+20]           #Sequence 20 bp+mutation
-    gkF = gkbp[1:21]                    #20bp before mutation
-    gkM = gkbp[0]                #Mutation with marker
-    i = lgkrF-1
+def print_rdetails(file_name,data1,data2,data3,data4,phd,lgkr,fwo,pos,lgkR):
+    gkbp = seq[lgkR-1:lgkR+21]           #Sequence 20 bp+mutation
+    gkM = gkbp[0]                    #20bp before mutation
+    gkF = gkbp[1:20]                #Mutation with marker
+    i = lgkR-1
     values = data1[pos[i]],data2[pos[i]],data3[pos[i]],data4[pos[i]]
     valueAv = sum(values)/len(values)
-    #print valueAv
     m=-1
     WT = None
     MT = None
+    WTrF = Seq(mutation[n][2])
+    WTrF = str(WTrF.reverse_complement())
+    gkmr = Seq(mutation[n][3])
+    gkmr = str(gkmr.reverse_complement())
     for j in values:
         m+=1
         if j >= valueAv*3/4:
             if fwo[m] is WTrF:
                 WT = '1'
-            elif fwo[m] is gkmr[n]:
+            elif fwo[m] is gkmr:
                 MT = '1'
             else:
                 continue
@@ -179,7 +191,7 @@ def print_rdetails(file_name,data1,data2,data3,data4,phd,WTrF,lgkrF,fwo):
     elif WT == None and MT == None:
         note = "Mutation not found"                
     print "\t\t"+mutation[n][0]+"  "+note+"\tG is "+str(data1[pos[i]])+  "   \tN2: "+WTrF+"\tQS: "+str(phd[i])+"\t"+gkM+" "+gkF
-    print "\t\t\t\tA is "+str(data2[pos[i]])+                   "   \tM:  "+mutation[n][3]
+    print "\t\t\t\tA is "+str(data2[pos[i]])+                   "   \tM:  "+gkmr
     print "\t\t\t\tT is "+str(data3[pos[i]])
     print "\t\t\t\tC is "+str(data4[pos[i]])
     print "------------------------------------------------------------------------------------------------"
@@ -196,25 +208,22 @@ for shortfn in files:
         continue
 
     fn = os.path.join(file_location, shortfn)
-    try:
-        for record in AbiTracer.AbiIterator(fn):
-            file_name = record.name         #File name
-            seq = Seq(record.seq)           #Sequence
-            rseq = seq.reverse_complement() #Reverse compliment of seq
-            phd = list(record.phd)          #Quality score converted to list
-            phdr = phd[::-1]                #Reverse phd list
-            fwo = record.FWO                #Filter Wheel Order = base order
-            data1 = record.DATA1            # fwo[0] = G
-            data2 = record.DATA2            # fwo[1] = A
-            data3 = record.DATA3            # fwo[2] = T
-            data4 = record.DATA4            # fwo[3] = C
-            data4r = data4[::-1]
-            pos = record.POS                #Base positions in trace files
-            posr = pos[::-1]                #Reverse base positions
-    except AbiTracer.ABIVersionError, e:
-        print 'error', e, fn
-		# continue
+    # Import file
+    for record in SeqIO.parse(fn,"abi"):
+        file_name = record.name         #File name
+        seq = record.seq           #Sequence
+        rseq = seq.reverse_complement() #Reverse compliment of seq
+        phd = list(record.letter_annotations["phred_quality"])          #Quality score converted to list
+        phdr = phd[::-1]                #Reverse phd list
+        data1 = record.data1            # fwo[0] = G
+        data2 = record.data2            # fwo[1] = A
+        data3 = record.data3            # fwo[2] = T
+        data4 = record.data4            # fwo[3] = C
+        data4r = data4[::-1]
+        pos = record.pos                #Base positions in trace files
+        posr = pos[::-1]                #Reverse base positions
     print shortfn
+    fwo = ("G","A","T","C") # define filter wheel order as tuple
 
     # gk loop in order to batch process all mutations
     n = -1
@@ -234,13 +243,10 @@ for shortfn in files:
             print_details(file_name,data1,data2,data3,data4,phd,WTf,lgkf,gkf,fwo,pos)
             continue
         elif lgkr >= 0:
-            gkr = gkn.reversecomplement()
-            gkr_ = gkr[n]
-            gkrF = gkr_[1:20]
-            WTrF = gkr_[0]
-            lgkrF = seq.find(gkrF)
-            if lgkrF >= 0:
-                print_rdetails(file_name,data1,data2,data3,data4,phd,WTrF,lgkrF,fwo)
+            gkr = gkf.reverse_complement()
+            lgkR = seq.find(gkr)
+            if lgkR >= 0:
+                print_rdetails(file_name,data1,data2,data3,data4,phd,lgkr,fwo,pos,lgkR)
                 continue
             continue
         else:
